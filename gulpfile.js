a127f9ba4bdca84156617f4f9efc2d2ea2aca5a7
doomymax <doomymax@gmail.com>
2020-09-22 23:44:31 +0300

    completed all section

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,6 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 const browserSync = require('browser-sync');
-// var concat = require('gulp-concat');
 
 gulp.task('pug', function() {
     return gulp.src('src/pug/index.pug')
@@ -41,11 +40,6 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('dist'))
 })
 
-// gulp.task('copy:html', function(){
-//   return gulp.src('src/index.html')
-//     .pipe(gulp.dest('dist'))
-// })
-
 gulp.task('copy:img', function() {
     return gulp.src('src/img/**/*.{png,jpg,jpeg,gif,svg}')
         .pipe(gulp.dest('dist/img'))
@@ -62,43 +56,43 @@ gulp.task('copy:fonts', function() {
  *  Объединяем все скрипты в один файл и делаем карту (опционально)
  *  Скрипты из папки vendor добавляются в первую очередь
  */
-gulp.task('javascript', function () {
-	return gulp.src()
-	.pipe(plumber())
-	.pipe(gulpif(true, sourcemaps.init()))
-	.pipe(gulpif(true, babel({
-		presets: ['@babel/preset-env']
-	})))
-	.pipe(concat('script.js'))
-	.pipe(gulpif(true, uglify()))
-	.pipe(gulpif(true, sourcemaps.write('../maps')))
-	.pipe(gulp.dest('dist/js'))
-	.pipe(browserSync.reload({
-		stream: true
-	}));
-});
+// gulp.task('javascript', function () {
+// 	return gulp.src()
+// 	.pipe(plumber())
+// 	.pipe(gulpif(true, sourcemaps.init()))
+// 	.pipe(gulpif(true, babel({
+// 		presets: ['@babel/preset-env']
+// 	})))
+// 	.pipe(concat('script.js'))
+// 	.pipe(gulpif(true, uglify()))
+// 	.pipe(gulpif(true, sourcemaps.write('../maps')))
+// 	.pipe(gulp.dest('dist/js'))
+// 	.pipe(browserSync.reload({
+// 		stream: true
+// 	}));
+// });
 
-gulp.task('javascript-vendor', function () {
-	return gulp.src('src/scripts/**/*.js')
-	.pipe(plumber())
-	.pipe(sourcemaps.init())
-	.pipe(babel({
-		presets: ['@babel/preset-env']
-    }))
-    .pipe(concat('script.min.js'))
-	.pipe(sourcemaps.write('.'))
-	.pipe(gulp.dest('dist/js'))
-});
+// gulp.task('javascript-vendor', function () {
+// 	return gulp.src('src/scripts/**/*.js')
+// 	.pipe(plumber())
+// 	.pipe(sourcemaps.init())
+// 	.pipe(babel({
+// 		presets: ['@babel/preset-env']
+//     }))
+//     .pipe(concat('script.min.js'))
+// 	.pipe(sourcemaps.write('.'))
+// 	.pipe(gulp.dest('dist/js'))
+// });
 
 gulp.task('watch', function() {
     gulp.watch('src/scss/**/*.scss', gulp.series('styles'))
-    gulp.watch('src/scripts/vendor/**/*.js', gulp.series('javascript-vendor'))
-    gulp.watch('src/scripts/vendor/**/*.js', gulp.series('javascript'))
+    // gulp.watch('src/scripts/vendor/**/*.js', gulp.series('javascript-vendor'))
+    // gulp.watch('src/scripts/vendor/**/*.js', gulp.series('javascript'))
     gulp.watch('src/pug/*.pug', gulp.series('pug'))
     gulp.watch('src/img/*', gulp.series('copy:img'))
-    gulp.watch(['src/scss/**/*.scss', 'src/pug/index.pug', 'src/img/**/*.{png,jpg,jpeg,gif,svg}', 'src/scripts/**/*.js']).on('change', bs.reload)
+    gulp.watch(['src/scss/**/*.scss', 'src/pug/index.pug', 'src/img/**/*.{png,jpg,jpeg,gif,svg}']).on('change', bs.reload)
 })
-
+// , 'src/scripts/**/*.js'
 gulp.task('bs', function() {
     bs.init({
         server: {
@@ -108,6 +102,6 @@ gulp.task('bs', function() {
 });
 
 gulp.task('default', gulp.parallel(
-    gulp.series('styles', 'pug', 'copy:img', 'copy:fonts', 'javascript', 'watch'),
+    gulp.series('styles', 'pug', 'copy:img', 'copy:fonts', 'watch'),
     'bs'
 ))
